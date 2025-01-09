bf16e3f3b93be7214c628bf114ceb0a4e31da453
LucasPort <lb.lucasbourdon@gmail.com>
2025-01-09 14:12:05 +0100

    création page 404

---
+++ src/views/404.html.php
@@ -0,0 +1,13 @@
+<div class=" w-full h-fit flex justify-center items-center ">
+    <div class="absolute z-50 top-[12rem] flex items-center justify-between w-fit h-[230px]">
+        <img src="./src/assets/medias/cloud.png" alt="cloud 1"
+            class="place-self-end drop-shadow-xl brightness-75 animate-cloud" width="180">
+        <img src="./src/assets/medias/cloud.png" alt="cloud 2"
+            class="place-self-start drop-shadow-xl brightness-20 animate-cloud" width="100">
+        <img src="./src/assets/medias/cloud.png" alt="cloud 3"
+            class="place-self-end drop-shadow-xl brightness-75 animate-cloud" width="80">
+        <img src="./src/assets/medias/cloud.png" alt="cloud 4" class="drop-shadow-xl brightness-75 animate-cloud"
+            width="150">
+    </div>
+    <h1 class="text-center text-[20rem] text-blue-500 drop-shadow-xl">404</h1>
+</div>
