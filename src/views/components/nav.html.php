4fc296f192b1925b9dcbf43d83d5abc09879fbeb
LucasPort <lb.lucasbourdon@gmail.com>
2025-01-09 14:13:12 +0100

    création du component nav

---
+++ src/views/components/nav.html.php
@@ -0,0 +1,11 @@
+<nav class="shadow-md flex align-middle justify-between h-fit">
+    <a href="/home">
+        <img src="./src/assets/medias/dailytrip.svg" width="60" alt="">
+    </a>
+
+    <ul class="flex gap-5 justify-between p-5">
+        <li><a href="#" class="font-semibold hover:text-blue-800">Home</a></li>
+        <li><a href="#" class="font-semibold hover:text-blue-800">trips</a></li>
+        <li><a href="#" class="font-semibold hover:text-blue-800">compte</a></li>
+    </ul>
+</nav>
