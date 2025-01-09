640e55ea31a34c1c09e0e1c5331c59af176a20d5
LucasPort <lb.lucasbourdon@gmail.com>
2025-01-09 15:31:43 +0100

    appèle de la bdd pour la création des tables et data pour l'insertion des tables

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 require_once './src/views/components/header.html.php';
 require_once './src/views/components/nav.html.php';
 
+require "bdd.php";
+require "data.php";
+
 require_once 'src/utils/Router.php';
 
 $router = new Router();
